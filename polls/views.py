3b124dfa357436327f980d5588aa3f25a285dbd2
slee272 <slee272@naver.com>
2022-11-02 00:11:05 +0900

    Modify checkbox issue

--- polls/views.py
+++ polls/views.py
@@ -25,7 +25,13 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        checked_list = request.POST.getlist('choice') #선택된 선택지들의 value를 리스트로 받아옴
+        selected_choices = [] #choice_set의 값을 저장할 리스트 생성
+
+        for i in range(len(checked_list)): #선택된 선택지의 개수만큼 반복
+            selected_choice = question.choice_set.get(pk=checked_list[i]) #선택된 값의 choice_set 값을 저장
+            selected_choices.append(selected_choice) #차례대로 리스트에 추가
+
     except(KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form
         return render(request, 'polls/detail.html', {
@@ -33,7 +39,8 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
+        for i in range(len(selected_choices)): #선택된 값의 개수만큼 반복
+            selected_choices[i].votes += 1 #선택된 값의 투표값을 1씩 증가시킨다
+            selected_choices[i].save() #투표 수를 저장한다
 
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
